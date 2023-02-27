2e0189a0adadf9a0f8b53403a94ac04701afca34
gintarevk <gintare.v.k@gmail.com>
2023-02-27 15:51:27 +0200

    Add Driver.java

---
+++ src/test/java/lt/gintare/pegasasbaigiamasisdarbas/pom/utils/Driver.java
@@ -0,0 +1,40 @@
+package lt.gintare.pegasasbaigiamasisdarbas.pom.utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+public class Driver {
+
+    // private static WebDriver driver;
+    private static ThreadLocal<WebDriver> driverThread = new ThreadLocal<>();
+
+    public static void setDriver() {
+
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        chromeOptions.addArguments("--start-maximized ");
+        chromeOptions.addArguments("--force-device-scale-factor=0.75");
+        // chromeOptions.addArguments("--headless");
+
+        // driver = new ChromeDriver(chromeOptions);
+        driverThread.set(new ChromeDriver(chromeOptions));
+        driverThread.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10)
+        );
+    }
+
+    public static WebDriver getDriver() {
+        return driverThread.get();
+    }
+
+    public static void closeDriver() {
+        driverThread.get().quit();
+        driverThread.remove();
+    }
+}
