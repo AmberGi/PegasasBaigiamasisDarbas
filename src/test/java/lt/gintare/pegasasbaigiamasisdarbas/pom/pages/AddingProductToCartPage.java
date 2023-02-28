636aed4419d9900e3577fa693ab137b26cd8400f
gintarevk <gintare.v.k@gmail.com>
2023-02-28 08:42:48 +0200

    Add class AddingProductToCartPage.java

---
+++ src/test/java/lt/gintare/pegasasbaigiamasisdarbas/pom/pages/AddingProductToCartPage.java
@@ -0,0 +1,30 @@
+package lt.gintare.pegasasbaigiamasisdarbas.pom.pages;
+
+public class AddingProductToCartPage {
+    public static void open() {
+        Common.openUrl("https://www.pegasas.lt");
+    }
+
+    public static void clickOnKnygos() {
+        Common.clickElement(
+                Locator.AddingProductToCartPage.meniuGroupKnygos
+        );
+    }
+
+    public static void clickOnNaujosKnygos() {
+
+
+    }
+
+    public static void clickOnBookMedineKauke() {
+
+    }
+
+    public static void clikOnAddMedineKaukeToCart() {
+
+    }
+
+    public static void clickOnCheckTheCart() {
+
+    }
+}
