9ecb528f9fd24a21abf3f65829fe35b5cb78af34
gintarevk <gintare.v.k@gmail.com>
2023-02-28 08:43:25 +0200

    Add class SortingProductsByMaxDiscountPage.java

---
+++ src/test/java/lt/gintare/pegasasbaigiamasisdarbas/pom/pages/SortingProductsByMaxDiscountPage.java
@@ -0,0 +1,7 @@
+package lt.gintare.pegasasbaigiamasisdarbas.pom.pages;
+
+public class SortingProductsByMaxDiscountPage {
+    public static void open() {
+        Common.openUrl("https://www.pegasas.lt/knygos/");
+    }
+}
