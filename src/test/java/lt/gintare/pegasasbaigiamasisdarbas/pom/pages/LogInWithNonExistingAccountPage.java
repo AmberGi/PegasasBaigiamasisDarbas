3c877cb189a6d8a1ddca1cf557e76fe853b465cb
gintarevk <gintare.v.k@gmail.com>
2023-02-28 08:42:18 +0200

    Add class LogInWithNonExistingAccountPage.java

---
+++ src/test/java/lt/gintare/pegasasbaigiamasisdarbas/pom/pages/LogInWithNonExistingAccountPage.java
@@ -0,0 +1,37 @@
+package lt.gintare.pegasasbaigiamasisdarbas.pom.pages;
+
+public class LogInWithNonExistingAccountPage {
+    public static void open() {
+        Common.openUrl("https://www.pegasas.lt");
+    }
+
+    public static void clickOnLoginRegister() {
+        Common.clickElement(
+                Locator.LogInWithNonExistingAccountPage.buttonLogIn
+        );
+    }
+
+    public static void enterTextToFieldEmail(String text) {
+        Common.sendKeysToElement(
+                Locator.MakeNewAccountPage.inputEmail, text
+        );
+    }
+
+    public static void enterTextToPassword(String text) {
+        Common.sendKeysToElement(
+                Locator.MakeNewAccountPage.inputPassword, text
+        );
+    }
+
+    public static void clickOnButtonPrisijungti() {
+        Common.clickElement(
+                Locator.MakeNewAccountPage.buttonPrisijungti
+        );
+    }
+
+    public static String readMessage() {
+        return Common.getElementText(
+                Locator.MakeNewAccountPage.paragraphMessageAlert
+        );
+    }
+}
