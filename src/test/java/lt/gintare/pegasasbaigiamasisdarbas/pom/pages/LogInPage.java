357b59181f2d0c1a4084348788e1ecd42a5403a6
gintarevk <gintare.v.k@gmail.com>
2023-02-28 12:02:55 +0200

    Renamed LogInPage.java

--- src/test/java/lt/gintare/pegasasbaigiamasisdarbas/pom/pages/LogInPage.java
+++ src/test/java/lt/gintare/pegasasbaigiamasisdarbas/pom/pages/LogInPage.java
@@ -1,6 +1,6 @@
 package lt.gintare.pegasasbaigiamasisdarbas.pom.pages;
 
-public class LogInWithNonExistingAccountPage {
+public class LogInPage {
     public static void open() {
         Common.openUrl("https://www.pegasas.lt");
     }
