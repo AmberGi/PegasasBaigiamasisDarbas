ae313dff5bfa6908f2b5509de6285be621ed709e
gintarevk <gintare.v.k@gmail.com>
2023-03-02 08:53:46 +0200

    Finished test GiftCardPurchingTest

--- src/test/java/lt/gintare/pegasasbaigiamasisdarbas/pom/pages/GiftCardPurchingPage.java
+++ src/test/java/lt/gintare/pegasasbaigiamasisdarbas/pom/pages/GiftCardPurchingPage.java
@@ -2,6 +2,49 @@ package lt.gintare.pegasasbaigiamasisdarbas.pom.pages;
 
 public class GiftCardPurchingPage {
     public static void open() {
-            Common.openUrl("https://www.pegasas.lt");
+        Common.openUrl("https://www.pegasas.lt"
+        );
+    }
+
+    public static void closeCookies() {
+        Common.clickElement(
+                Locator.GiftCardPurchingPage.closeCookiesbutton
+        );
+    }
+
+    public static void clickOnDovanuKuponai() {
+        Common.clickElement(
+                Locator.GiftCardPurchingPage.buttonDovanuKuponai
+        );
+    }
+
+    public static void clickOnDovanuKuponasVaikiskasTamsus() {
+        Common.clickElement(
+                Locator.GiftCardPurchingPage.buttonDovanuKuponasTamsus
+        );
+    }
+
+    public static void clickOnPasirinkiteSuma5Eurai() {
+        Common.clickElement(
+                Locator.GiftCardPurchingPage.buttonPasirinktiSuma5Eurai
+        );
+    }
+
+    public static void clickOnIdekiteKuponaIKrepseli() {
+        Common.clickElement(
+                Locator.GiftCardPurchingPage.buttonIdetiKuponaIKrepseli
+        );
+    }
+
+    public static void clickOnPerziurekiteKrepseli() {
+        Common.clickElement(
+                Locator.GiftCardPurchingPage.buttonPerziurekiteKrepseliWithGiftCard
+        );
+    }
+
+    public static String dovanuKuponasVaikiskasTamsusIsInTheCart() {
+        return Common.getElementText(
+                Locator.GiftCardPurchingPage.buttonDovanuKuponasTamsusIsInTheCart
+        );
     }
 }
--- src/test/java/lt/gintare/pegasasbaigiamasisdarbas/pom/tests/GiftCardPurchingTest.java
+++ src/test/java/lt/gintare/pegasasbaigiamasisdarbas/pom/tests/GiftCardPurchingTest.java
@@ -1,6 +1,7 @@
 package lt.gintare.pegasasbaigiamasisdarbas.pom.tests;
 
 import lt.gintare.pegasasbaigiamasisdarbas.pom.pages.GiftCardPurchingPage;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -10,11 +11,26 @@ public class GiftCardPurchingTest extends BaseTest {
     @Override
     public void setUp() {
         GiftCardPurchingPage.open();
+        GiftCardPurchingPage.closeCookies();
     }
 
     @Test
-    public void testAddingGiftCardToCart() {
+    public void testAddBlackDizainGiftCardToCart() {
 
+        String expectedResult = "El. dovanų kuponas vaikiškas (tamsus)";
+        String actualResult;
 
+        GiftCardPurchingPage.clickOnDovanuKuponai();
+        GiftCardPurchingPage.clickOnDovanuKuponasVaikiskasTamsus();
+        GiftCardPurchingPage.clickOnPasirinkiteSuma5Eurai();
+        GiftCardPurchingPage.clickOnIdekiteKuponaIKrepseli();
+        GiftCardPurchingPage.clickOnPerziurekiteKrepseli();
+
+        actualResult = GiftCardPurchingPage.dovanuKuponasVaikiskasTamsusIsInTheCart();
+
+        Assert.assertTrue(
+                actualResult.contains(expectedResult),
+                String.format("Actual: %s; Expected: %s", actualResult, expectedResult)
+        );
     }
 }
